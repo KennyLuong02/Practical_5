d72fe73665dd71557bd5b57a6eee609c2035ac0a
Dai Luong <luong@DESKTOP-INLVFFC.localdomain>
2023-09-04 17:35:12 +0930

    Hi

--- main.cpp
+++ main.cpp
@@ -6,24 +6,24 @@
 using namespace std;
 
 int main() {
-    std::vector<int> list;
+    // std::vector<int> list;
 
-    list.push_back(-3);
-    list.push_back(1);
-    list.push_back(0);
-    list.push_back(10);
-    list.push_back(7);
-    list.push_back(2);
-    list.push_back(6);
+    // list.push_back(-3);
+    // list.push_back(1);
+    // list.push_back(0);
+    // list.push_back(10);
+    // list.push_back(7);
+    // list.push_back(2);
+    // list.push_back(6);
 
-    QuickSort q;
-    list = q.sort(list);
+    // QuickSort q;
+    // list = q.sort(list);
 
-    for (int i = 0; i < 7; i++) {
-        cout << list.at(i) << " ";
-    }
+    // for (int i = 0; i < 7; i++) {
+    //     cout << list.at(i) << " ";
+    // }
 
-    cout << endl;
+    // cout << endl;
 
     return 0;
 

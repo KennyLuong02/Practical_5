889aebae2b12348b6ea385a8a8d023591f4ed0f1
Dai Luong <luong@DESKTOP-INLVFFC.localdomain>
2023-09-04 14:36:57 +0930

    Hi

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #include "BubbleSort.h"
 #include "Sort.h"
 
- vector<int>BubbleSort::sort(vector<int> list){
+ vector<int> BubbleSort::sort(vector<int> list){
     bubbleSort(list);
     return list;
 
@@ -17,6 +17,8 @@ void BubbleSort::bubbleSort(vector<int>& array){
      for (int i = array.size() - 1; i > 0; i--) {
           for(int j = 0; j < i; j++) {
           if(array.at(j) > array.at(j+1)) {
+            // swap array.at(j) and swap array.at(j+1)
+            swap(array.at(j), array.at(j+1));
          } 
         }
       }
